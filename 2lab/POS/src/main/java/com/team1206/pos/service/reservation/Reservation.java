e74abe12240978f784391eb130075e1844f1cb0f
Motiejus Šveikauskas <118689022+Motiejukaas@users.noreply.github.com>
2024-12-09 17:54:18 +0200

    added Reservation model

---
+++ 2lab/POS/src/main/java/com/team1206/pos/service/reservation/Reservation.java
@@ -0,0 +1,53 @@
+package com.team1206.pos.service.reservation;
+
+import com.team1206.pos.service.service.Service;
+import com.team1206.pos.user.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "\"reservation\"")
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "first_name", nullable = false, length = 100)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 100)
+    private String lastName;
+
+    @Column(name = "phone", length = 20, nullable = true)
+    private String phone;
+
+    @Column(name = "appointed_at", nullable = false)
+    private LocalDateTime appointedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "service_id", nullable = false)
+    private Service service;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private User employee;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at", nullable = true)
+    private LocalDateTime updatedAt;
+
+    @PreUpdate
+    public void setUpdatedAt() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
