22bce5320a9af09818336f4ae8ce63880df28a74
AluminumAlman <Almantas1122334455@gmail.com>
2024-12-14 15:28:56 +0200

    Add many-to-many relations to discount entity

--- 2lab/POS/src/main/java/com/team1206/pos/payments/discount/Discount.java
+++ 2lab/POS/src/main/java/com/team1206/pos/payments/discount/Discount.java
@@ -1,11 +1,18 @@
 package com.team1206.pos.payments.discount;
 
+import com.team1206.pos.inventory.product.Product;
+import com.team1206.pos.inventory.productCategory.ProductCategory;
+import com.team1206.pos.inventory.productVariation.ProductVariation;
+import com.team1206.pos.order.order.Order;
+import com.team1206.pos.order.orderItem.OrderItem;
+import com.team1206.pos.service.service.Service;
 import com.team1206.pos.user.merchant.Merchant;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -37,6 +44,45 @@ public class Discount {
     @ManyToOne
     @JoinColumn(name = "merchant_id", nullable = false)
     private Merchant merchant;
+
+    @ManyToMany
+    @JoinTable(name = "discounts_product_categories",
+            joinColumns = @JoinColumn(name = "discount_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_category_id"))
+    private List<ProductCategory> productCategories;
+
+    @ManyToMany
+    @JoinTable(name = "discounts_products",
+            joinColumns = @JoinColumn(name = "discount_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id"))
+    private List<Product> products;
+
+    @ManyToMany
+    @JoinTable(name = "discounts_product_variations",
+            joinColumns = @JoinColumn(name = "discount_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_variation_id"))
+    private List<ProductVariation> productVariations;
+
+    @ManyToMany
+    @JoinTable(name = "discounts_services",
+            joinColumns = @JoinColumn(name = "discount_id"),
+            inverseJoinColumns = @JoinColumn(name = "service_id"))
+    private List<Service> services;
+
+    @ManyToMany
+    @JoinTable(name = "discounts_orders",
+            joinColumns = @JoinColumn(name = "discount_id"),
+            inverseJoinColumns = @JoinColumn(name = "order_id"))
+    private List<Order> orders;
+
+    @ManyToMany
+    @JoinTable(name = "discounts_order_items",
+            joinColumns = @JoinColumn(name = "discount_id"),
+            inverseJoinColumns = @JoinColumn(name = "order_item_id"))
+    private List<OrderItem> orderItems;
+
+    @Column(name = "isActive")
+    private Boolean isActive;
 
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
