3e87c8b33612ef37a69dd40d23a39dc70636b94a
Evgenij Shapovalov <zhenya.shapovalov@gmail.com>
2024-12-19 22:18:49 +0200

    add closeOrder method and automation on new transactions

--- 2lab/POS/src/main/java/com/team1206/pos/payments/transaction/TransactionService.java
+++ 2lab/POS/src/main/java/com/team1206/pos/payments/transaction/TransactionService.java
@@ -128,7 +128,10 @@ public class TransactionService {
         Transaction savedTransaction = transactionRepository.save(transaction);
 
         BigDecimal totalPaid = getTotalPaidByOrder(orderId);
-        if(totalPaid >= )
+
+        // Atkomentuot kai bus orderService.toPay
+//        if(totalPaid >= orderService.toPay(orderId))
+//            orderService.closeOrder(orderId);
 
         return mapToResponseDTO(savedTransaction);
     }
