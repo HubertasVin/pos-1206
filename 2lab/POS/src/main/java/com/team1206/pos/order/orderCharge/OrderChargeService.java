529a9a1962042af640451fbfa047d7953d403d82
AluminumAlman <Almantas1122334455@gmail.com>
2024-12-19 18:36:19 +0200

    Add endpoints for adding and removing order charges

--- 2lab/POS/src/main/java/com/team1206/pos/order/orderCharge/OrderChargeService.java
+++ 2lab/POS/src/main/java/com/team1206/pos/order/orderCharge/OrderChargeService.java
@@ -1,14 +1,16 @@
 package com.team1206.pos.order.orderCharge;
 
 import com.team1206.pos.common.enums.OrderChargeType;
+import com.team1206.pos.common.enums.OrderStatus;
 import com.team1206.pos.common.enums.ResourceType;
+import com.team1206.pos.exceptions.IllegalRequestException;
 import com.team1206.pos.exceptions.ResourceNotFoundException;
 import com.team1206.pos.exceptions.UnauthorizedActionException;
 import com.team1206.pos.order.order.Order;
 import com.team1206.pos.order.order.OrderService;
 import com.team1206.pos.user.merchant.Merchant;
 import com.team1206.pos.user.user.UserService;
-import jakarta.validation.Valid;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -75,21 +77,62 @@ public class OrderChargeService {
         Order order = orderService.getOrderEntityById(orderId);
         userService.verifyLoggedInUserBelongsToMerchant(order.getMerchant().getId(), "You are not authorized to delete charges from this order");
 
-        OrderCharge orderCharge = orderChargeRepository.findById(chargeId)
-                                                       .orElseThrow(() -> new ResourceNotFoundException(
-                                                               ResourceType.ORDER_CHARGE,
-                                                               chargeId.toString()
-                                                       ));
-
+        OrderCharge orderCharge = getOrderChargeEntityById(chargeId);
         /*if (!orderCharge.getOrder().getId().equals(orderId)) {
             throw new ResourceNotFoundException(ResourceType.ORDER_CHARGE, chargeId.toString());
         }*/
-
         orderChargeRepository.delete(orderCharge);
+    }
+
+    @Transactional
+    public void addOrderChargeToOrder(UUID chargeId, UUID orderId) {
+        OrderCharge orderCharge = getOrderChargeEntityById(chargeId);
+        userService.verifyLoggedInUserBelongsToMerchant(orderCharge.getMerchant().getId(),
+                "You are not authorized to add order charge to this order");
+
+        Order order = orderService.getOrderEntityById(orderId);
+        userService.verifyLoggedInUserBelongsToMerchant(order.getMerchant().getId(),
+                "You are not authorized to add order charge to this order");
+
+        if (order.getStatus() != OrderStatus.OPEN)
+            throw new IllegalRequestException("Order has to be open to add order charge");
+
+        if (orderCharge.getOrders().contains(order))
+            throw new IllegalRequestException("Order charge has already been added to this order");
+
+        orderCharge.getOrders().add(order);
+        orderChargeRepository.save(orderCharge);
+    }
+
+    @Transactional
+    public void removeOrderChargeFromOrder(UUID chargeId, UUID orderId) {
+        OrderCharge orderCharge = getOrderChargeEntityById(chargeId);
+        userService.verifyLoggedInUserBelongsToMerchant(orderCharge.getMerchant().getId(),
+                "You are not authorized to add order charge to this order");
+
+        Order order = orderService.getOrderEntityById(orderId);
+        userService.verifyLoggedInUserBelongsToMerchant(order.getMerchant().getId(),
+                "You are not authorized to add order charge to this order");
+
+        if (order.getStatus() != OrderStatus.OPEN)
+            throw new IllegalRequestException("Order has to be open to add order charge");
+
+        if (!orderCharge.getOrders().remove(order))
+            throw new IllegalRequestException("Order charge has already been added to this order");
+
+        orderChargeRepository.save(orderCharge);
     }
 
 
     // *** Helper methods ***
+    public OrderCharge getOrderChargeEntityById(UUID orderChargeId) {
+        OrderCharge orderCharge = orderChargeRepository.findById(orderChargeId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        ResourceType.ORDER_CHARGE,
+                        orderChargeId.toString()));
+
+        return orderCharge;
+    }
 
     private void setOrderChargeFields(OrderCharge orderCharge, OrderChargeRequestDTO requestBody) {
         orderCharge.setType(OrderChargeType.valueOf(requestBody.getType().toUpperCase()));
