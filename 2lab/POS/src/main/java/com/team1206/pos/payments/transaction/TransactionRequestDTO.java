2506ac7df694db65cafb7ebed36a5a78bee7f3bb
HubertasVin <hubertas.personal@proton.me>
2024-12-20 19:25:23 +0200

    fix

--- 2lab/POS/src/main/java/com/team1206/pos/payments/transaction/TransactionRequestDTO.java
+++ 2lab/POS/src/main/java/com/team1206/pos/payments/transaction/TransactionRequestDTO.java
@@ -2,6 +2,7 @@ package com.team1206.pos.payments.transaction;
 
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -14,7 +15,7 @@ public class TransactionRequestDTO {
     @Pattern(regexp = "^(cash|payment_card|gift_card)$", message = "paymentMethod must be either 'cash', 'payment_card' or 'gift_card'")
     private String paymentMethodType;
 
-    @NotBlank
+    @NotNull
     @DecimalMin(value = "0.01", inclusive = false, message = "Price must be greater than 0.01")
     private BigDecimal amount;
 }
